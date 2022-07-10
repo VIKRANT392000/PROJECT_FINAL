8f3b4ddbd4d4a32db6d5a038b51a8b3283897825
VIKRANT392000 <65649429+VIKRANT392000@users.noreply.github.com>
2022-07-10 20:28:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 import streamlit as st
 from multiapp import MultiApp
+import matplotlib
+import seaborn
+import plotly
 from apps import home, sentiment_2 ,sentiment_1, sentiment_0
 
 app = MultiApp()
